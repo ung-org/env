f4797b8b4549cd91b6c6b7d76b5e21aabbfc409e
Jakob Kaivo <jkk@ung.org>
2019-08-06 08:55:18 -0400

    update license, minor cleanup

--- env.c
+++ env.c
@@ -1,24 +1,30 @@
 /*
  * UNG's Not GNU
  *
- * Copyright (c) 2011-2017, Jakob Kaivo <jkk@ung.org>
+ * Copyright (c) 2011-2019, Jakob Kaivo <jkk@ung.org>
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#define _XOPEN_SOURCE 700
+#define _XOPEN_SOURCE 500
 #include <errno.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,10 +33,11 @@
 #define UTILITY_NOT_INVOKED (126)
 #define UTILITY_NOT_FOUND (127)
 
-extern char **environ;
-
 int main(int argc, char *argv[])
 {
+	setlocale(LC_ALL, "");
+	extern char **environ;
+
 	int c;
 	while ((c = getopt(argc, argv, "i")) != -1) {
 		switch (c) {
@@ -58,6 +65,6 @@ int main(int argc, char *argv[])
 	
 	execvp(argv[optind], argv + optind);
 
-	fprintf(stderr, "env: Couldn't run '%s': %s\n", argv[optind], strerror(errno));
+	fprintf(stderr, "env: %s: %s\n", argv[optind], strerror(errno));
 	return errno == ENOENT ? UTILITY_NOT_FOUND : UTILITY_NOT_INVOKED;
 }
